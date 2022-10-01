1a23ad620faf5b936ed61f397fce1aeca0263e3a
Ucastro20 <112229103+Ucastro20@users.noreply.github.com>
2022-10-01 15:46:37 -0500

    [ADD] Agregue el Comando que da la hora

---
+++ Hora.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#Asignando el Color del texto
+Rojo='\033[31m'
+
+#Creando variables para los valores
+Hr=$(date +%H) #Variable con la hora actual
+Min=$(date +%M) #Variable con el minuto actual
+Seg=$(date +%S) #Variable con el segundo actual
+
+#Muestra de la hora al Usuario
+printf "$Rojo"
+echo "La hora del sistema es $Hr:$Min:$Seg"
