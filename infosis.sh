6e82fa79613b82273d0b56e6ccd390ef483b6d5b
Alan Salvador Chilpa Navarro <87553596+Chilpa19@users.noreply.github.com>
2022-10-05 12:56:11 -0500

    [INFOSIS] Modifique errores en arquitectura

--- infosis.sh
+++ infosis.sh
@@ -10,7 +10,6 @@ cat /proc/meminfo | grep 'MemFree' | cut -c 18-30
 printf "La memoria total de la ram del sistema es: "
 cat /proc/meminfo | grep 'MemAvailable' | cut -c 18-30
 printf "La arquitectura del sistema es: "
-cat /etc/apt/sources.list | grep "Linux" | grep "Release" 
+cat /etc/apt/sources.list | grep "Linux" | cut -d ' ' -f8
 printf "La versión del SO es: "
 cat /etc/os-release | grep 'PRETTY_NAME' | cut -c 13-30
-
