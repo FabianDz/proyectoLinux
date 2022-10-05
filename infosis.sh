e64f9cec31383590842c366273c0f91ae8630417
Alan Salvador Chilpa Navarro <87553596+Chilpa19@users.noreply.github.com>
2022-10-05 12:00:09 -0500

    [INFOSIS] Agregue infosis

---
+++ infosis.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+Violeta='\e[1;35m'
+
+printf "$Violeta"
+printf "La memoria total de la ram del sistema es: "
+cat /proc/meminfo | grep 'MemTotal' | cut -c 18-30
+printf "La memoria libre de la ram del sistema es: "
+cat /proc/meminfo | grep 'MemFree' | cut -c 18-30
+printf "La memoria total de la ram del sistema es: "
+cat /proc/meminfo | grep 'MemAvailable' | cut -c 18-30
+printf "La arquitectura del sistema es: "
+cat /etc/apt/sources.list | grep "Linux" | grep "Release" 
+printf "La versión del SO es: "
+cat /etc/os-release | grep 'PRETTY_NAME' | cut -c 13-30
+
