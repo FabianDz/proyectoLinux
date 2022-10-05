9a2487f4984bf258579f357cca8df3cd1d97126a
Alan Salvador Chilpa Navarro <87553596+Chilpa19@users.noreply.github.com>
2022-10-05 13:23:36 -0500

    [INFOSIS] Corrección de texto

--- infosis.sh
+++ infosis.sh
@@ -7,7 +7,7 @@ printf "La memoria total de la ram del sistema es: "
 cat /proc/meminfo | grep 'MemTotal' | cut -c 18-30
 printf "La memoria libre de la ram del sistema es: "
 cat /proc/meminfo | grep 'MemFree' | cut -c 18-30
-printf "La memoria total de la ram del sistema es: "
+printf "La memoria ocupada de la ram del sistema es: "
 cat /proc/meminfo | grep 'MemAvailable' | cut -c 18-30
 printf "La arquitectura del sistema es: "
 cat /etc/apt/sources.list | grep "Linux" | cut -d ' ' -f8
