8ff5e695471210ec8ff05d9a65de5a54a3838154
Ucastro20 <112229103+Ucastro20@users.noreply.github.com>
2022-10-04 19:42:14 -0500

    [FIX] Comando Fecha y Hora

--- Hora.sh
+++ Hora.sh
@@ -3,12 +3,19 @@
 #Asignando el Color del texto
 Rojo='\033[31m'
 
-#Creando variables para los valores
-Hr=$(date +%H) #Variable con la hora actual
-Min=$(date +%M) #Variable con el minuto actual
-Seg=$(date +%S) #Variable con el segundo actual
-Fecha=$(date +%F)
 
-#Muestra de la hora al Usuario
+
+
+
+#Muestra la  fecha y hora actuales
 printf "$Rojo"
-echo "La hora del sistema es: $Fecha $Hr:$Min:$Seg"
+printf "La hora del sistema es: "
+cat /proc/driver/rtc | grep 'rtc_time' | grep "[0-9]*:[0-9]*:[0-9]*" -o
+printf "La fecha del sistema es: "
+cat /proc/driver/rtc | grep 'rtc_date' | grep "[0-9]*-[0-9]*-[0-9]*" -o
+
+#/proc/driver/rtc es el archivo que mantiene el tiempo del sistema aun si esta apagado '
+#Las lineas 'rtc_date' y 'rtc_time' nos dan la fecha y hora del sistema
+#El segundo grep le indicamos el formato en el que estan los datos
+#El formato de la fecha es: AAAA-MM-DD
+#El formato de la hora es: HH:MM:SS
